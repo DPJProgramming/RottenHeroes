3a441277a4541513046a7e29b503244219a423cd
cmatthews444 <155667279+cmatthews444@users.noreply.github.com>
2024-05-09 12:51:55 -0700

    home page index

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+//328
+
+//for error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$f3 = Base::instance();
+
+$f3->route('GET /', function() {
+    //echo '<h1>test test</h1>';
+
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
