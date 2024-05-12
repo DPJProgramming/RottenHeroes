b54a2b0deec5eb529e21cae841961968eb3137ac
cmatthews444 <155667279+cmatthews444@users.noreply.github.com>
2024-05-12 16:22:57 -0700

    Changed Composer to fit all three of us. Moved index to a point where I need other pages. Finished most routing

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <?php
+// Colton Matthews 5/12/24
+//Most of this is dependent on the home page and will change.
 
-//for error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+require_once ('vendor/autoload.php');
+
 $f3 = Base::instance();
 
-$f3->route('GET /', function() {
-    //echo '<h1>test test</h1>';
-
+$f3->route('GET /', function($f3) {
     $view = new Template();
     echo $view->render('views/home.html');
 });
 
-
-// Log-in
-$f3->route('GET /views/login', function() {
-    //echo '<h1>My Breakfast Menu</h1>';
-
-    // Render a view page
-    $view = new Template();
-    echo $view->render('views/login.html');
+$f3->route('GET|POST /login', function($f3) {
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    } else {
+        $view = new Template();
+        echo $view->render('views/login.html');
+    }
 });
+
+$f3->route('GET|POST /signup', function($f3) {
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    } else {
+        $view = new Template();
+        echo $view->render('views/signUp.html');
+    }
+});
+
+$f3->route('GET /hero/@name', function($f3, $params) {
+    $view = new Template();
+    echo $view->render('views/hero.html');
+});
+
+
+$f3->run();
