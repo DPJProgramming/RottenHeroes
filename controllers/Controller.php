7c243cb67d76d8749b3dd3398c96f966ecebabe2
dpjprogramming <cheesyd64@hotmail.com>
2024-05-28 22:39:08 -0400

    modified index to use Controller class for routing

---
+++ controllers/Controller.php
@@ -0,0 +1,35 @@
+<?php
+
+class Controller
+{
+    private $_f3;
+
+    function __construct($f3)
+    {
+        $this->_f3 = $f3;
+    }
+
+    function home()
+    {
+        $view = new Template();
+        echo $view->render('views/home.html');
+    }
+
+    function hero()
+    {
+        $view = new Template();
+        echo $view->render('views/hero.html');
+    }
+
+    function login()
+    {
+        $view = new Template();
+        echo $view->render('views/login.html');
+    }
+
+    function signUp()
+    {
+        $view = new Template();
+        echo $view->render('views/signUp.html');
+    }
+}
--- index.php
+++ index.php
@@ -5,39 +5,36 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once ('vendor/autoload.php');
+require_once('vendor/autoload.php');
+require_once('controllers/Controller.php');
 
 $f3 = Base::instance();
+$con = new Controller($f3);
 
 $f3->route('GET /', function($f3) {
-    $view = new Template();
-    echo $view->render('views/home.html');
+    $GLOBALS['con']->home();
 });
 
 $f3->route('GET|POST /login', function($f3) {
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
     } else {
-        $view = new Template();
-        echo $view->render('views/login.html');
+        $GLOBALS['con']->login();
     }
 });
 
 $f3->route('GET|POST /signup', function($f3) {
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
     } else {
-        $view = new Template();
-        echo $view->render('views/signUp.html');
+        $GLOBALS['con']->signUp();
     }
 });
 
 $f3->route('GET /hero', function($f3, $params) {
-    $view = new Template();
-    echo $view->render('views/hero.html');
+    $GLOBALS['con']->hero();
 });
 
 $f3->route('GET /home', function($f3) {
-    $view = new Template();
-    echo $view->render('views/home.html');
+    $GLOBALS['con']->home();
 });
 
 $f3->run();
